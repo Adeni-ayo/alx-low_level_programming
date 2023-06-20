484a684871dfb716d06f994c1024e976b44da0f7
Adeni-ayo <adeniayodeji@gmail.com>
2023-06-19 23:19:11 -0700

    4-puts.c5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - a c program cthat prints a comment
+ *
  * Return: Always 0 (success)
- */=
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
